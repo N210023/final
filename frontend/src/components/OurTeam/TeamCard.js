ab7d85065dbf47316ed1c38de588d59f927cc242
N210023 <n210023@rguktn.ac.in>
2025-04-14 16:27:06 +0530

    Update TeamCard.js

--- frontend/src/components/OurTeam/TeamCard.js
+++ frontend/src/components/OurTeam/TeamCard.js
@@ -2,46 +2,22 @@
 import React from "react";
 import "./Team";
 
-const TeamCard = ({ name, title, id }) => {
+const TeamCard = ({ name, title, id, call }) => {
   return (
     <div className="team-card">
       <img src={`https://intranet.rguktn.ac.in/SMS/usrphotos/user/${id}.jpg`} alt={name} className="team-image" />  
       <h3>{name}</h3>
       <h4>{title}</h4>
       <div className="social-icons">
-      <a href="https://instagram.com" target="_blank" rel="noreferrer" className="icon" id="icon-1">
-  <i className="fab fa-instagram"></i>
-</a>
-<a href="https://twitter.com" target="_blank" rel="noreferrer" className="icon" id="icon-3">
-  <i className="fab fa-twitter"></i>
-</a>
-<a href="https://facebook.com" target="_blank" rel="noreferrer" className="icon" id="icon-4">
-  <i className="fab fa-facebook"></i>
-</a>
-<a href="https://github.com" target="_blank" rel="noreferrer" className="icon" id="icon-2">
-  <i className="fab fa-github"></i>
+      <a href="https://instagram.com" target="_blank" className="icon" id="icon-1"><i class="fab fa-instagram"></i></a>
+            <a href="https://twitter.com" target="_blank" className="icon" id="icon-3"><i class="fab fa-twitter"></i></a>
+            <a href="https://facebook.com" target="_blank" className="icon" id="icon-4"><i class="fab fa-facebook"></i></a>
+            <a href="https://github.com" target="_blank" className="icon" id="icon-2">
+  <i class="fab fa-github"></i>
 </a>
 
-
-<a
-  href="https://in.linkedin.com/in/pandi-srinivas-siddartha-256767316"
-  target="_blank"
-  rel="noreferrer"
-  className="icon"
-  id="icon-5"
->
-  <i className="fab fa-linkedin fa-1.5x"></i>
-</a>
-
-<a
-  href="https://wa.me/919059533659"
-  target="_blank"
-  rel="noreferrer"
-  className="icon"
-  id="icon-7"
->
-  <i className="fab fa-whatsapp"></i>
-</a>
+            <a href="https://in.linkedin.com/in/pandi-srinivas-siddartha-256767316" target="_blank" className="icon" id="icon-5"><i class="fab fa-linkedin fa-1.5x"></i></a>
+            <a href= {call} target="_blank" class="icon" id="icon-7"><i className="fab fa-whatsapp"></i></a>
       </div>
     </div>
   );
