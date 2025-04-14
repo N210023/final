0f31e2bd0278473c7959d44d9428b3ad74ec99ba
N210023 <n210023@rguktn.ac.in>
2025-04-14 16:53:06 +0530

    Update Login.js

--- frontend/src/components/Login/Login.js
+++ frontend/src/components/Login/Login.js
@@ -27,7 +27,7 @@ const Login = ({ setUserName }) => {
   const handleVoterLogin = async (e) => {
     e.preventDefault();
     try {
-      const response = await fetch("http://localhost:5000/api/auth/login", {
+      const response = await fetch("https://final-hmhf.onrender.com/api/auth/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email: voterEmail, password: voterPassword }),
