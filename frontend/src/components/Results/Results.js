6ae86b85678f6b6f2a8545433241175c4e545d9b
N210023 <n210023@rguktn.ac.in>
2025-04-14 16:54:03 +0530

    Update Results.js

--- frontend/src/components/Results/Results.js
+++ frontend/src/components/Results/Results.js
@@ -11,7 +11,7 @@ const Results = () => {
     const getCandidates = async () => {
       try {
         const response = await fetch(
-          "http://localhost:5000/api/vote/candidates"
+          "https://final-hmhf.onrender.com/api/vote/candidates"
         ); // Adjust your backend URL
         const data = await response.json();
         setCandidates(data); // Store the fetched candidates in state
