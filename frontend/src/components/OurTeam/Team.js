1b3061484b1c78e3420ebe712f57065c94409de0
N210023 <n210023@rguktn.ac.in>
2025-04-14 16:26:42 +0530

    Update Team.js

--- frontend/src/components/OurTeam/Team.js
+++ frontend/src/components/OurTeam/Team.js
@@ -5,12 +5,12 @@ import memberImage from "./member.png"; // Importing the image
 import "./Team.css";
 
 const teamMembers = [
-  { name: "Diwakar", title: "Scrum manager", id:"N210242" },
-  { name: "Karthik", title: "Full Stack Developer",id:"N210803" },
-  { name: "Siddartha", title: "Product Manager", id:"N210023" },
-  { name: "Aditya", title: "Web Developer", id:"N210435" },
-  { name: "Farhana", title: "Web Developer", id:"N210459" },
-  { name: "Manoj", title: "Web Developer", id:"N210519" },
+  { name: "Diwakar", title: "Scrum manager", id:"N210242",call: "https://wa.me/918074416044"},
+  { name: "Karthik", title: "Full Stack Developer",id:"N210803",call:"https://wa.me/918121227279"},
+  { name: "Siddartha", title: "Product Manager", id:"N210023",call:"https://wa.me/919059533659" },
+  { name: "Aditya", title: "Web Developer", id:"N210435", call:"https://wa.me/919059533659"},
+  { name: "Farhana", title: "Web Developer", id:"N210459", call:"https://wa.me/919059533659"},
+  { name: "Manoj", title: "Web Developer", id:"N210519",call:"https://wa.me/917416587543"}
 ];
 
 const Team = () => {
@@ -23,6 +23,7 @@ const Team = () => {
             name={member.name}
             title={member.title}
             id={member.id}
+            call={member.call}
           />
         ))}
       </div>
